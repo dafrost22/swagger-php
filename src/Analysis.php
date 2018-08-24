8dbb968a75f7c79a2425195129fbd5b8f6403df3
Ross Williams <ross@solodev.com>
2018-08-24 00:09:08 -0400

    just another test

--- src/Analysis.php
+++ src/Analysis.php
@@ -221,8 +221,7 @@ class Analysis
 
     public function getUsedTraits($class)
     {
-        print($class);
-        if (!is_string($class) || !class_exists($class)) {
+        if (empty($class) || !class_exists($class)) {
             return [];
         }
         $usedTraits = class_uses($class);
